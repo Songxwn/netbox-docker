a830eb9fadf7f11781c94ec2ea900fe4e485ee46
IP9527 <21145071+Songxwn@users.noreply.github.com>
2024-04-28 13:58:38 +0800

    Update extra.py

--- configuration/extra.py
+++ configuration/extra.py
@@ -47,3 +47,6 @@
 # from datetime import datetime
 # now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 # BANNER_TOP = f'<marquee width="200px">This instance started on {now}.</marquee>'
+ENABLE_LOCALIZATION = True
+TIME_ZONE = 'Asia/Shanghai'
+
